be262b90bdc0455126d587de561e738c6df48f76
Marcin Plinta <marcin.plinta93@gmail.com>
2022-11-17 23:43:38 +0100

    Add Firebase

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -19,32 +20,16 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Najlepsza Pizza w Cieszynie')),
       body: Builder(builder: (context) {
         if (currentIndex == 0) {
-          return Center(
-            child: Text('Jeden'),
-          );
+          return const RestaurantPageContent();
         }
         if (currentIndex == 1) {
-          return Center(
-            child: Text('Dwa'),
-          );
+          return const AddOpinionContent();
         }
 
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Jesteś zalogowany jako ${widget.user.email}'),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                  onPressed: () {
-                    FirebaseAuth.instance.signOut();
-                  },
-                  child: const Text('Wyloguj'))
-            ],
-          ),
-        );
+        return MyAccountContent(email: widget.user.email);
       }),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
@@ -60,6 +45,114 @@ class _HomePageState extends State<HomePage> {
               icon: Icon(Icons.person), label: 'Moje konto'),
         ],
       ),
+    );
+  }
+}
+
+class RestaurantPageContent extends StatelessWidget {
+  const RestaurantPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance.collection('restaurant').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Wystąpił nieoczekiwany problem'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Proszę czekać, trwa ładowanie danych');
+          }
+
+          final documents = snapshot.data!.docs;
+
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Restauracja'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(document['name']),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Pizza'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(document['pizza']),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Ocena'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(document['rating'].toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
+  }
+}
+
+class MyAccountContent extends StatelessWidget {
+  const MyAccountContent({
+    Key? key,
+    required this.email,
+  }) : super(key: key);
+
+  final String? email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Jesteś zalogowany jako $email'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: const Text('Wyloguj'))
+        ],
+      ),
+    );
+  }
+}
+
+class AddOpinionContent extends StatelessWidget {
+  const AddOpinionContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Dwa'),
     );
   }
 }
