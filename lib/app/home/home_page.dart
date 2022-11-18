bb4c0fe8437ddbe347320ded4aef18e3672afcb5
Marcin Plinta <marcin.plinta93@gmail.com>
2022-11-18 17:31:30 +0100

    Implement adding restaurants

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pizza_cieszyn/app/home/add_oppinion/add_oppinion_page_content.dart';
+import 'package:pizza_cieszyn/app/home/my_account/my_account_page_content.dart';
+import 'package:pizza_cieszyn/app/home/restaurant/restaurant_page_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -45,114 +47,6 @@ class _HomePageState extends State<HomePage> {
               icon: Icon(Icons.person), label: 'Moje konto'),
         ],
       ),
-    );
-  }
-}
-
-class RestaurantPageContent extends StatelessWidget {
-  const RestaurantPageContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance.collection('restaurant').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Wystąpił nieoczekiwany problem'));
-          }
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text('Proszę czekać, trwa ładowanie danych');
-          }
-
-          final documents = snapshot.data!.docs;
-
-          return ListView(
-            children: [
-              for (final document in documents) ...[
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Restauracja'),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(document['name']),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Pizza'),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(document['pizza']),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text('Ocena'),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(document['rating'].toString()),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ],
-          );
-        });
-  }
-}
-
-class MyAccountContent extends StatelessWidget {
-  const MyAccountContent({
-    Key? key,
-    required this.email,
-  }) : super(key: key);
-
-  final String? email;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Jesteś zalogowany jako $email'),
-          const SizedBox(height: 20),
-          ElevatedButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text('Wyloguj'))
-        ],
-      ),
-    );
-  }
-}
-
-class AddOpinionContent extends StatelessWidget {
-  const AddOpinionContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
     );
   }
 }
---
+++ lib/app/home/restaurant/restaurant_page_content.dart
@@ -0,0 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantPageContent extends StatelessWidget {
+  const RestaurantPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance.collection('restaurant').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Wystąpił nieoczekiwany problem'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Proszę czekać, trwa ładowanie danych');
+          }
+
+          final documents = snapshot.data!.docs;
+
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Restauracja'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(document['name']),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Pizza'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(document['pizza']),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Ocena'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(document['rating'].toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
+  }
+}
